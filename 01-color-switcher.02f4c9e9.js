81a58dfe2487d1e0cc8f9522aff1604b21c09f9c
SerhiiRytikov <111732658+SerhiiRytikov@users.noreply.github.com>
2023-02-16 23:04:04 +0000

    Deploying to gh-pages from @ SerhiiRytikov/goit-js-hw-09@982f9137dadd4737c2c66ce12f2b210768dc8762 🚀

---
+++ 01-color-switcher.02f4c9e9.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");document.querySelector("body");let r=null;t.addEventListener("click",(()=>{r=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.setAttribute("disabled",!0)})),e.addEventListener("click",(()=>{clearInterval(r),t.removeAttribute("disabled"),e.setAttribute("disabled",!0)}));
+//# sourceMappingURL=01-color-switcher.02f4c9e9.js.map
