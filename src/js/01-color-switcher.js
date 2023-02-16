982f9137dadd4737c2c66ce12f2b210768dc8762
SerhiiRytikov <serg_ritikov@ukr.net>
2023-02-17 01:03:40 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+//* ===============================
+//* функція зміни рандом кольора
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+//* ===============================
+//* доступ до кнопок та боді(для зміни кольору боді)
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+//* змінна для запуску та зупинки інтервалу (з конспекту)
+let timerId = null;
+
+//* запускає таймер та зміну кольору фону боді з інтервалом 1 секунда (1000 міллісекунд)
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  //* деактивує кнопку start (disabled)
+  startBtn.setAttribute('disabled', true);
+});
+
+//* зупиняє таймер
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  //* видаляє деактивацію з кнопки start (disabled)
+  startBtn.removeAttribute('disabled');
+  //* деактивує кнопку stop (disabled) як на відео з тз
+  stopBtn.setAttribute('disabled', true);
+});
